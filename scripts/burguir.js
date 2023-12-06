417f2184da084256a8af3e26132ab1e0b53bc6ad
DavidCounagoGonzalez <dcounagogonzalez@danielcastelao.org>
2023-12-06 13:35:42 +0100

    Burguir acabada

--- scripts/burguir.js
+++ scripts/burguir.js
@@ -1,29 +1,15 @@
 window.addEventListener("load", inicio);
 
 function inicio(){
-const desplegable = document.querySelector(".desplegable");
-const items = document.querySelectorAll(".itemMenu");
-const burguir = document.querySelector(".hamburguesa");
-const closeIcon = document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const nav = document.getElementById("nav");
+const abrir = document.getElementById("abrir");
+const cerrar = document.getElementById("cerrar");
 
-function desplega(){
-    if(desplegable.classList.contains("showMenu")){
-        desplegable.classList.remove("showMenu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    }else{
-        desplegable.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
-}
+abrir.addEventListener("click", () =>{
+    nav.classList.add("visible");
+})
 
-burguir.addEventListener("click", desplega);
-
-items.forEach(
-    function(item){
-        item.addEventListener("click", desplega);
-    }
-)
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
 }
