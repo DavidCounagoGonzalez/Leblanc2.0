483b9a1fa06d8d006c37968e271f1db23ea2e0b9
DavidCounagoGonzalez <dcounagogonzalez@danielcastelao.org>
2023-12-06 17:44:20 +0100

    Carrousel terminado

---
+++ scripts/carrousel.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', inicio);
+
+function inicio(){
+    const slider = document.querySelector(".slider--inner");
+
+    let imagenes = slider.querySelectorAll("img");
+
+    let index = 1;
+
+    setInterval(function(){
+        let porcentaje = index * -100;
+        slider.style.transform = "translateX(" + porcentaje + "%)";
+        index++;
+        if(index >= imagenes.length){
+            index = 0;
+        }
+    }, 5000)
+}
